d67122659355506ffdf3411163d277bc7ad80315
Mukul Kumar <mukulsharma1998@gmail.com>
2022-09-25 13:28:41 -0400

    Removed all errors need to give id in frontend and fetch through req.getParameter

--- SOEN6441-API/src/main/java/services/UserServlet.java
+++ SOEN6441-API/src/main/java/services/UserServlet.java
@@ -1,5 +1,6 @@
 package services;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import java.sql.SQLException;
@@ -8,20 +9,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import bean.UserDetails;
+import dao.UserDetailDAO;
 import dao.UserDetailDAOImpl;
-import jakarta.servlet.http;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 
 public class UserServlet extends HttpServlet {
   
   private UserDetailDAOImpl userDao = UserDetailDAOImpl.getInstance();
   private static final Logger LOGGER = Logger.getLogger(UserServlet.class.getName());
 
-  @Override
   protected void doPost(HttpServletRequest req, HttpServletRequest resp) throws ServletException, IOException {
 
   }
 
-  @Override
   protected void doGet(HttpServletRequest req, HttpServletRequest response) throws ServletException, IOException {
     String action = req.getServletPath();
     try {
@@ -30,9 +36,9 @@ public class UserServlet extends HttpServlet {
           getUsers(req, response);
           break;
         default:
-          response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+          ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_NOT_FOUND);
           RequestDispatcher dispatcher = req.getRequestDispatcher("/user");
-          dispatcher.include(req,response);
+          dispatcher.include(req,(ServletResponse) response);
       }
     } catch (SQLException e) {
       LOGGER.log(Level.SEVERE, "SQL Error", e);
@@ -40,19 +46,28 @@ public class UserServlet extends HttpServlet {
   }
 
   private void getUsers(HttpServletRequest req, HttpServletRequest response) throws SQLException, ServletException, IOException{
-    int pageNumber = req.getParameter("pageNumber");
-    int pageSize = req.getParameter("pageSize");
-    List<UserDetails> users = userDao.readUser(pageNumber, pageSize);
+    String pageNumber = req.getParameter("pageNumber");
+    String pageSize = req.getParameter("pageSize");
+    List<UserDetails> users = null;
+	try {
+		users = userDao.readUser();
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
-    PrintWriter out = response.getWriter();
-    response.setContentType("application/json");
+    PrintWriter out = ((ServletResponse) response).getWriter();
+    ((ServletResponse) response).setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
     out.print(users);
     out.flush();  
   }
 
   private void createeUser(HttpServletRequest req, HttpServletRequest response) throws SQLException, ServletException, IOException{
-    int id = req.getParameter("userId");
+	int id = req.getParameter("id");; //Dummy Id
     String firstName = req.getParameter("firstName");
     String lastName = req.getParameter("lastName");
     String email = req.getParameter("email");
@@ -60,8 +75,8 @@ public class UserServlet extends HttpServlet {
     boolean success = userDao.insertUser(user);
     String responseMessage = success ? "Succesfully created user" : "Failed to create user";
     
-    PrintWriter out = response.getWriter();
-    response.setContentType("application/json");
+    PrintWriter out = ((ServletResponse) response).getWriter();
+    ((ServletResponse) response).setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
     out.print(responseMessage);
     out.flush();  
@@ -76,8 +91,8 @@ public class UserServlet extends HttpServlet {
     boolean success = userDao.updateUser(user);
     String responseMessage = success ? "Succesfully updated user details" : "Failed to update user details";
     
-    PrintWriter out = response.getWriter();
-    response.setContentType("application/json");
+    PrintWriter out = ((ServletResponse) response).getWriter();
+    ((ServletResponse) response).setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
     out.print(responseMessage);
     out.flush();  
@@ -88,8 +103,8 @@ public class UserServlet extends HttpServlet {
     boolean success = userDao.deleteUser(id);
     String responseMessage = success ? "Succesfully deleted user" : "Failed to delete user";
     
-    PrintWriter out = response.getWriter();
-    response.setContentType("application/json");
+    PrintWriter out = ((ServletResponse) response).getWriter();
+    ((ServletResponse) response).setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
     out.print(responseMessage);
     out.flush();  
